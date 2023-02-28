2c2f5a72a53e903579e3bf10e2a8e8403ab2aa3d
MutiaraSyafitri11 <syafitrimutiara20@gmail.com>
2023-02-28 12:00:03 +0700

    tambah splash dan intro

--- app/src/main/java/com/example/foodorderapp/Activity/IntroActivity.kt
+++ app/src/main/java/com/example/foodorderapp/Activity/IntroActivity.kt
@@ -1,12 +1,25 @@
 package com.example.foodorderapp.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.foodorderapp.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        next()
+    }
+
+    fun next() {
+        val btnNext: Button = findViewById(R.id.btn_sign)
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
